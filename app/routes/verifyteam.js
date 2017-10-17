0a1f70ed20a210e3697c52adf183c28d249af022
Brian Wisnowski <brian_wisnowski@cuttingedgeinfo.com>
2017-10-17 15:32:57 -0400

    created verifidation page for addgame/team on one page added a little css

--- app/routes/verifyteam.js
+++ app/routes/verifyteam.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
-  model: function() {
-	return this.store.query('teamstat', {
-		orderBy: 'id',
-		limitToLast: 1
-	});
-  }
+  model() {
+	return RSVP.hash ({teamstat: this.store.query('teamstat', {
+		orderBy: 'id', limitToLast: 1}),
+		addgame: this.store.query('addgame', {
+		orderBy: 'id', limitToLast: 1
+	 })
+
+});
+}
 });
